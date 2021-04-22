37908a801d6e5d819742e4b6d93ba7ea08d871e3
Jason Taisey <jdtaisey89@icloud.com>
2021-04-22 06:57:53 -0700

    fixed the expected results for the test

--- javascript/code-challenges/Arrays/array-binary-search/__tests__/array-binary-search.test.js
+++ javascript/code-challenges/Arrays/array-binary-search/__tests__/array-binary-search.test.js
@@ -7,12 +7,12 @@ const BinarySearch = require('../array-binary-search.js');
 // TEST CHALLENGE DAY 03
 describe('Testing challenge Array-Binary-Shift', () => {
   test('It should return an interger that matches the value within the arrays', () => {
-    expect(BinarySearch([0, 2, 6, 8, 10, 12, 15, 22, 34, 55], 15)).toStrictEqual([6]);
+    expect(BinarySearch([0, 2, 6, 8, 10, 12, 15, 22, 34, 55], 15)).toStrictEqual(6);
   });
 });
 describe('Testing challenge Array-Binary-Shift', () => {
   test('If the key is not in the array, return -1', () => {
-    expect(BinarySearch([1, 5, 8, 9, 10, 13, 17, 22], 15)).toStrictEqual([-1]);
+    expect(BinarySearch([1, 5, 8, 9, 10, 13, 17, 22], 15)).toStrictEqual(-1);
   });
 });
 
